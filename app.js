bfcb39e04b3c06ed4650c8b3668f178cf639b134
rakib <rakibulislamr83@gmail.com>
2025-07-31 23:56:56 +0600

    env update

--- app.js
+++ app.js
@@ -4,33 +4,25 @@ const connectDb = require("./db/connect");
 require("dotenv").config();
 
 const port = process.env.PORT || 5000;
-//  const cors = require("cors");
+
 
 const products_routes = require("./routes/products");
 
-// middleware
-// app.use(cors());
 app.use("/api/products", products_routes);
 
 app.get("/", (req, res) => {
   res.send("E-Commerce server Is Running");
 });
-// app.get("/allProducts", (req, res) => {
-//   res.send(productData);
-// });
 
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`);
-// });
 const startServer = async () => {
   try {
-    await connectDb()(); // Call the connectDB function to establish a connection
+    await connectDb(process.env.MONGO_URL); 
     app.listen(port, () => {
       console.log(`Server is running on port ${port}`);
     });
   } catch (error) {
     console.error("Failed to start the server:", error.message);
-    process.exit(1); // Exit the process with failure
+    process.exit(1); 
   }
 };
 
--- db/connect.js
+++ db/connect.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = () => async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URL);
     console.log("MongoDB connected successfully");
   } catch (error) {
     console.error("MongoDB connection failed:", error.message);
@@ -10,3 +10,4 @@ const connectDB = () => async () => {
   }
 };
 module.exports = connectDB;
+ 
